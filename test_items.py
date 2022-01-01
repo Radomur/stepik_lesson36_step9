4b132f3a2047b9eb29cae9e2306c491189eeaed0
Radomur <kkashirsk@yandex.ru>
2022-01-01 23:21:57 +0200

    delete_timer

--- test_items.py
+++ test_items.py
@@ -1,7 +1,5 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-import time
 
 def test_presence_of_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
     assert browser.find_element_by_css_selector("button.btn-add-to-basket"), "button not found"
